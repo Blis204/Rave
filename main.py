bd2eaf162158fd20c16bbcc01cf369f2ac4dd11d
BuildTools <nikdraws@users.noreply.github.com>
2022-12-23 14:15:06 +0100

    Added leaderboard

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, pages
 from discord.commands.context import ApplicationContext
 from discord.commands import Option
 import aiosqlite
 import asyncio
+from easy_pil import Canvas, Editor, Font, load_image
 import time
 import random
 from dotenv import load_dotenv, find_dotenv
@@ -24,6 +25,41 @@ async def on_ready():
                                 (user INT PRIMARY KEY NOT NULL, xp INT, level INT, nextlevel INT, background VARCHAR(30), time INT, total INT, name VARCHAR(50))""")
 
 
+@bot.slash_command(name="leaderboard", description="Get the xp leaderboard")
+async def leaderboard(ctx: ApplicationContext):
+    async with bot.db.cursor() as cursor:
+        await cursor.execute("SELECT total, user FROM levels ORDER BY total DESC")
+        data = await cursor.fetchall()
+
+        strings = []
+        author_string = ""
+        pos = 1
+        p = 1
+        for d in data:
+            f_string = f"#{pos}: <@{d[1]}> XP: `{d[0]}`"
+            strings.append(f_string)
+            if d[1] == ctx.author.id:
+                author_string += f"**#{pos}: <@{d[1]}> XP: `{d[0]}`**"
+            pos += 1
+
+        chunks = [strings[i: i + 10] for i in range(0, len(strings), 10)]
+        embeds = []
+        for chunk in chunks:
+            texts = []
+            for n in chunk:
+                texts.append(f"{n}")
+            names = "\n".join(texts)
+            line = "-"*47
+            e = discord.Embed(title=f"**{ctx.guild.name} Leaderboard**",
+                              description=f"{names}\n{line}\n{author_string}", color=0x97ce4c)
+            e.set_footer(text=f"Page: {p}/{len(chunks)}")
+            p += 1
+            embeds.append(e)
+        paginator = pages.Paginator(
+            pages=embeds, timeout=30, disable_on_timeout=True)
+        await paginator.respond(ctx.interaction)
+
+
 @bot.slash_command(name="rank", description="Shows your rank")
 async def rank(ctx: ApplicationContext, user: Option(discord.User,
                                                      "Select a user to get the rank from",
@@ -35,7 +71,7 @@ async def rank(ctx: ApplicationContext, user: Option(discord.User,
         user_id = user.id
         user_name = user.name
     async with bot.db.cursor() as cursor:
-        await cursor.execute("SELECT xp, level, nextlevel FROM levels WHERE user =?", (user_id,))
+        await cursor.execute("SELECT xp, level, nextlevel, background FROM levels WHERE user =?", (user_id,))
         data = await cursor.fetchone()
 
     if not data:
@@ -44,12 +80,10 @@ async def rank(ctx: ApplicationContext, user: Option(discord.User,
         else:
             await ctx.respond("This user doesn't have a rank yet!", ephemeral=True)
     else:
-        embed = discord.Embed(
-            title=f"{user_name} Rank!", color=0x377BB8)
-        embed.add_field(name="XP", value=f"`{data[0]}`", inline=True)
-        embed.add_field(name="Required", value=f"`{data[2]}`", inline=True)
-        embed.add_field(name="Level", value=f"`{data[1]}`", inline=True)
-        await ctx.respond(embed=embed)
+        xp = data[0]
+        level = data[1]
+        goal = data[2]
+        image = data[3]
 
 
 @bot.event
